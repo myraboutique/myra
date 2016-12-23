c2a81809f298b6ba48993485f33452bdeebad99c
mukesh.r <mukesh.r@productivet.com>
2016-12-23 20:18:17 +0530

    UI changes fixed

--- config/dev.js
+++ config/dev.js
@@ -10,7 +10,7 @@ module.exports = (function(){
             host: 'localhost',
             port: 27017,
             user: 'root',
-            password: 'root',
+            password: '123456',
             database: 'myra'
         },
         logging: {
