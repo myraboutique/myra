24f9b7aad375816b58943e674af01a3eeae33376
ankur.p <ankur.p@productivet.com>
2017-01-24 19:25:11 +0530

    material chnages

--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,26 @@
+// module.exports = (function(){
+//     var m = {
+//         server: {
+//             host: 'localhost',
+//             port: 3000,
+//             ssl: false
+//         },
+//         db: {
+//             type: 'mysql',
+//             host: '85.10.205.173',
+//             port: 3307,
+//             user: 'myraboutique',
+//             password: 'myra@123',
+//             database: 'myra'
+//         },
+//         logging: {
+//             level: "info"
+//         }
+//     };
+
+//     return m;
+// })();
+
 module.exports = (function(){
     var m = {
         server: {
@@ -7,11 +30,11 @@ module.exports = (function(){
         },
         db: {
             type: 'mysql',
-            host: '85.10.205.173',
-            port: 3307,
-            user: 'myraboutique',
-            password: 'myra@123',
-            database: 'myra'
+            host: 'localhost',
+            port: 3306,
+            user: 'root',
+            password: 'root',
+            database: 'demo1'
         },
         logging: {
             level: "info"
