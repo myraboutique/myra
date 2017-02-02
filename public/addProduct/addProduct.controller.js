9d78d9a2b412808e104d2e2cc570da75cac25208
Mahammad <mahammadc_productivet@outlook.com>
2017-02-02 20:08:31 +0530

    addproduct

--- public/addProduct/addProduct.controller.js
+++ public/addProduct/addProduct.controller.js
@@ -7,22 +7,24 @@ function addProductController($resource,$state) {
   var vm = this;
   vm.image = [];
   vm.images = [];
-  // vm.measurements = [];
-// console.log(vm.measurements);
+  
   var measurement = $resource('/api/measurement');
   measurement.query(function (info) {
     vm.type = info;
-    vm.selected =  vm.type[0].title;
-    info.forEach(function (e){
-      vm.image = e.image.split('###');
-      vm.images.push(vm.image);
-      // vm.measurements.push = e.measurement.split(',');
-    });
   });
+
+  
 
   vm.typeSelect = typeSelect;
   function typeSelect(info) {
-    
+    vm.measurements = [];
+    // vm.style1 = info[0].title;
+    vm.type.forEach(function (e){
+      if(e.title == info)
+        vm.measurements.push(e.measurement);
+    });
+    vm.measure = vm.measurements[0].split(',');
+    console.log(vm.measure[0]);
   };  
 
   var addmaterial = $resource('/api/addmaterial')
@@ -30,6 +32,17 @@ function addProductController($resource,$state) {
       vm.material = info;
       // console.log(vm.material);   
   });
+
+  var customerdetails = $resource('/api/customerdetails')
+  customerdetails.query(function (info) {
+    vm.data = info[2];
+    vm.measureWAIST = info[2].measureWAIST;
+    vm.measureBUST = info[2].measureBUST;
+     vm.measureSH = info[2].measureSH;
+     vm.measureLWAIST = info[2].measureLWAIST;
+    // console.log(vm.measureWAIST);
+  });
+
 }
      
 
