ea624953ddbbddd42fb662fbf0c7e1ccf07339fd
Mahammad <mahammadc_productivet@outlook.com>
2017-02-03 13:07:15 +0530

    conflict

--- db/orderdetails.js
+++ db/orderdetails.js
@@ -1,12 +1,12 @@
 /// <reference path="../typings/tsd.d.ts" />
 
-module.exports: (function () {
-  var models: require('../models').orderdetails;
-  var db: require('../core/db');
-  var sequelize: require('sequelize');
+module.exports = (function () {
+  var models = require('../models').orderdetails;
+  var db = require('../core/db');
+  var sequelize = require('sequelize');
 
 
-  var m: {
+  var m = {
     find: function (req, res) {
       db.sync().then(function () {
         models.findAll().then(function (info) {
