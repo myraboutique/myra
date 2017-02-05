ce30579f98f4d0e96abeaa352f958aac03bfc106
Mahammad <mahammadc_productivet@outlook.com>
2017-02-06 04:55:28 +0530

    dfghjk

--- public/addProduct/addProduct.controller.js
+++ public/addProduct/addProduct.controller.js
@@ -1,43 +1,44 @@
 angular.module('myra')
   .controller('addProductController', addProductController);
 
-addProductController.$inject = ['$resource','$state','$http'];
+addProductController.$inject = ['$resource', '$state', '$http'];
 
-function addProductController($resource,$state,$http) {
+function addProductController($resource, $state, $http) {
   var vm = this;
 
-  vm.data1=localStorage.getItem('editproduct');
+  vm.data1 = localStorage.getItem('editproduct');
   vm.data = JSON.parse(vm.data1);
 
-  vm.selectimage = function(index) {
-    vm.index = index;
-  }
+  // vm.selectimage = function(index) {
+  //   vm.index = index;
+  // }
 
   vm.designSelect = designSelect;
   vm.submit = submit;
-  
+
   var measurement = $resource('/api/measurement');
   var addsubdesigns = $resource('/api/addsubdesign');
   var addmaterial = $resource('/api/addmaterial');
   var customerdetails = $resource('/api/customerdetails');
   var orderdetails = $resource('/api/orderdetails');
-  
+  var Summary = $resource('/api/summary/:id');
 
   measurement.query(function (info) {
     vm.type = info;
-
   });
 
   addsubdesigns.query(function (info) {
     vm.type2 = info;
-    // console.log(vm.type2);  
   });
 
-  
-
-  addmaterial.query(function(info){
-      vm.material = info; 
+  addmaterial.query(function (info) {
+    vm.material = info;
   });
+
+  // Summary.query({ id: 1 }, function (info) {
+  //   vm.order2 = info;
+  //   console.log(vm.order2);
+  // });
 
   customerdetails.query(function (info) {
     vm.measureWAIST = info[0].measureWAIST;
@@ -61,69 +62,67 @@ function addProductController($resource,$state,$http) {
     vm.measureMORI = info[0].measureMORI;
     vm.measureCROSS = info[0].measureCROSS;
   });
-  
+
   function designSelect(info) {
+
     vm.measurements = [];
     vm.measure = [];
     vm.images1 = [];
     vm.images1title = [];
-    
-    vm.type.forEach(function (e){
-      if(e.title == info) {
+
+    vm.type.forEach(function (e) {
+      if (e.title == info) {
         vm.measurements.push(e.measurement);
-        vm.ee = e.id;
+        // vm.ee = e.id;
       }
     });
 
-    vm.type2.forEach(function (e){
-      if(e.design == info) {
+    vm.type2.forEach(function (e) {
+      if (e.design == info) {
         vm.images1.push(e.subdesignimage);
         vm.images1title.push(e.subdesign);
       }
     });
     vm.measure1 = vm.measurements[0].split(',');
     vm.measure.push("");
-    vm.measure1.forEach(function(element) {
+    vm.measure1.forEach(function (element) {
       vm.measure.push(element.trim());
     }, this);
-  };  
+  };
 
   function submit(info) {
-      info.measureWAIST = vm.measureWAIST;
-      info.measureWAIST = vm.measureWAIST;
-      info.measureBUST = vm.measureBUST;
-      info.measureSH = vm.measureSH;
-      info.measureLWAIST = vm.measureLWAIST;
-      info.measureHIPS = vm.measureHIPS;
-      info.measureSLEEVES = vm.measureSLEEVES;
-      info.measureSHORT = vm.measureSHORT;
-      info.measuretype = vm.measuretype;
-      info.measureLENGTH = vm.measureLENGTH;
-      info.measureFULL = vm.measureFULL;
-      info.measureFULLL = vm.measureFULLL;
-      info.measureKNEE = vm.measureKNEE;
-      info.measureARMHOLE = vm.measureARMHOLE;
-      info.measureUTHIGH = vm.measureUTHIGH;
-      info.measureLTHIGH = vm.measureLTHIGH;
-      info.measureCALF = vm.measureCALF;
-      info.measureFNECK = vm.measureFNECK;
-      info.measureBNECK = vm.measureBNECK;
-      info.measureMORI = vm.measureMORI;
-      info.measureCROSS = vm.measureCROSS;
-      info.id = vm.data.id;
+    info.measureWAIST = vm.measureWAIST;
+    info.measureWAIST = vm.measureWAIST;
+    info.measureBUST = vm.measureBUST;
+    info.measureSH = vm.measureSH;
+    info.measureLWAIST = vm.measureLWAIST;
+    info.measureHIPS = vm.measureHIPS;
+    info.measureSLEEVES = vm.measureSLEEVES;
+    info.measureSHORT = vm.measureSHORT;
+    info.measuretype = vm.measuretype;
+    info.measureLENGTH = vm.measureLENGTH;
+    info.measureFULL = vm.measureFULL;
+    info.measureFULLL = vm.measureFULLL;
+    info.measureKNEE = vm.measureKNEE;
+    info.measureARMHOLE = vm.measureARMHOLE;
+    info.measureUTHIGH = vm.measureUTHIGH;
+    info.measureLTHIGH = vm.measureLTHIGH;
+    info.measureCALF = vm.measureCALF;
+    info.measureFNECK = vm.measureFNECK;
+    info.measureBNECK = vm.measureBNECK;
+    info.measureMORI = vm.measureMORI;
+    info.measureCROSS = vm.measureCROSS;
+    info.id = vm.data.id;
 
 
-     $http.put('/api/orderdetails', info)
-       .then(
-       function (response) {
-         window.location = '#/subdesign';
-       },
-       function (response) {
-
-       });
-
-    
+    $http.put('/api/orderdetails', info)
+      .then(
+      function (response) {
+        window.location = '#/subdesign';
+      },
+      function (response) {
+      });
   }
 }
-     
+
 
