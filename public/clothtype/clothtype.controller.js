3ac1086690b74e73e6f3027b36aafdeee0aa22af
Mahammad <mahammadc_productivet@outlook.com>
2017-02-17 10:32:39 +0530

    temp

--- public/clothtype/clothtype.controller.js
+++ public/clothtype/clothtype.controller.js
@@ -6,10 +6,9 @@ clothtypeController.$inject = ['$resource','$state','$scope'];
 function clothtypeController($resource,$state,$scope) {
   var vm = this;
 
-  // vm.image = [];
-  // vm.images = [];
   vm.editpage = editpage;
   vm.order = order;
+  vm.dynamicmeasure = [[]];
 
   vm.temp = function(data) {
     $scope.index = data;
@@ -28,15 +27,34 @@ function clothtypeController($resource,$state,$scope) {
   }
 
   var measurement = $resource('/api/measurement');
+  var managemeasurements = $resource('/api/managemeasurements');
+  
+  managemeasurements.query(function(info){
+    vm.manamgemeasure = info;
+  });
+
   measurement.query(function(info){
-    vm.type = info;
-    // console.log(vm.type);
-    // info.forEach(function (e){
-    //   vm.mesu1 = e.measurement;
-    //   vm.mesu = vm.mesu1.split(',');
-    //   vm.measure.push(vm.mesu); 
-    // });
-    // console.log(vm.measure);
+    vm.type = info;  
+
+    for (var index = 0; index < info.length; index++) {
+      vm.m = info[index].measurement.split(',');
+      vm.m.forEach(function(element2) {
+      vm.dynamicmeasure[index].push(vm.manamgemeasure[element2].name);
+        console.log(vm.dynamicmeasure[index]);
+      }, this);  
+    }  
+
+    // info.forEach(function(element) {
+
+    //   vm.m = element.measurement.split(',');
+
+    //   vm.m.forEach(function(element2) {
+    //     vm.dynamicmeasure[0].push(vm.manamgemeasure[element2].name);
+    //   }, this);
+    // console.log(vm.dynamicmeasure[0]);
+
+    // }, this);
+
   });
 
    function editpage(x)
