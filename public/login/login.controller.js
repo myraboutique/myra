e2cc46c4d10a4cb02e87b9981842e31b953480b0
Mahammad <mahammadc_productivet@outlook.com>
2017-02-18 11:58:34 +0530

    deploye

--- public/login/login.controller.js
+++ public/login/login.controller.js
@@ -7,34 +7,31 @@ function loginController($resource,$state) {
 
   var vm = this;
   vm.data = data;
-  var Login = $resource('/api/register');
-  Login.query(function (info) {
-    vm.Login = info;
-  });
-  function setup(x){
-    if(x.isActive==1){
-           localStorage.setItem('token',JSON.stringify(x));
-       // window.location = '#/home';
-        $state.go('home'); 
-    }
-    else{
-      vm.validationmsg="Username might be inactive.";
-    }   
+  var Login = $resource('/api/login');
 
-  }
   function data(myform){
     vm.formSubmitted = true;
     if(myform.$valid){
-     for(var i=0;i<=vm.Login.length;i++){
-      if(vm.Login[i].username==vm.username &&  vm.Login[i].password==vm.password){        
-          setup(vm.Login[i]);
-      }          
-     }
- 
-      
-     }
-  }  
+    var login = new Login();
+    login.username = vm.username;
+    login.password = vm.password;
+    login.$save(function(info){
+      if(info.username){
+        if(info.isActive==1){
+          localStorage.setItem('token',JSON.stringify(info));
+          // window.location = '#/home';
+          $state.go('home');
+        }
+        else{
+          vm.validationmsg = "Username might be inactive.";
+        }
 
+      } else {
+        vm.validationmsg = "Username and/or Password does not match!";
+      } 
+    });
+  }
 
+}
 
 }
