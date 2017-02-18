28957e297e455496eca777e7821541a7accce5a5
Mahammad <mahammadc_productivet@outlook.com>
2017-02-18 16:41:12 +0530

    AD

--- public/subdesign/addnew/addsubdesign.controller.js
+++ public/subdesign/addnew/addsubdesign.controller.js
@@ -38,7 +38,8 @@ var addsubdesign = new AddSubDesign();
     
     vm.upload = function (file) {
         Upload.upload({
-            url: 'https://myraboutique.herokuapp.com/upload', //webAPI exposed to upload the file
+            // url: 'https://myraboutique.herokuapp.com/upload', //webAPI exposed to upload the file
+            url: 'http://localhost:3000/upload', //webAPI exposed to upload the file
             data:{file:file} //pass file as data, should be user ng-model
         }).then(function (resp) { //upload function returns a promise
             if(resp.data.error_code === 0){ //validate success
