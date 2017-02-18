2df2194d5bfe5ee1f5475e5f41f761dc05a3325d
Mahammad <mahammadc_productivet@outlook.com>
2017-02-18 21:14:56 +0530

    kbkv

--- public/addProduct/addProduct.controller.js
+++ public/addProduct/addProduct.controller.js
@@ -207,8 +207,8 @@ function addProductController($resource, $state, $http, Upload, $window) {
 
   vm.upload = function (file,cb) {
     Upload.upload({
-      // url: 'https://myraboutique.herokuapp.com/upload', //webAPI exposed to upload the file
-      url: 'http://localhost:3000/upload', //webAPI exposed to upload the file
+      url: 'http://myraboutique.herokuapp.com/upload', //webAPI exposed to upload the file
+      // url: 'http://localhost:3000/upload', //webAPI exposed to upload the file
       data: { file: file } //pass file as data, should be user ng-model
     }).then(function (resp) { //upload function returns a promise
       if (resp.data.error_code === 0) { //validate success
