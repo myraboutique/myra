1e3e8f353d18a29ebb78501da02bf3d77882d558
mukesh.r <mukesh.r@productivet.com>
2017-02-18 18:53:29 +0530

    18feb final changes

--- config/dev.js
+++ config/dev.js
@@ -10,8 +10,8 @@ module.exports = (function(){
             host: 'localhost',
             port: 3306,
             user: 'root',
-            password: 'root',
-            database: 'demo2'
+            password: '123456',
+            database: 'demo1'
         },
         logging: {
             level: "info"
@@ -19,3 +19,25 @@ module.exports = (function(){
     };
     return m;
 })();
+
+// module.exports = (function(){
+//     var m = {
+//         server: {
+//             host: 'localhost',
+//             port: 3000,
+//             ssl: false
+//         },
+//         db: {
+//             type: 'mysql',
+//             host: '85.10.205.173',
+//             port: 3307,
+//             user: 'myraboutique',
+//             password: 'myra@123',
+//             database: 'myra'
+//         },
+//         logging: {
+//             level: "info"
+//         }
+//     };
+//    return m;
+// })();
--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 ﻿//module.exports = require('./config.' + process.env.NODE_ENV);
-// module.exports = require('./dev');
-module.exports = require('./dev1');
+module.exports = require('./dev');
+// module.exports = require('./dev1');
