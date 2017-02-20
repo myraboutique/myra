5b43acc46a3b861b2ba9f8f8901ca6492200e6ce
hardik.l <hardik.l@productivet.com>
2017-02-20 13:56:55 +0530

    local

--- public/test/test.controller.js
+++ public/test/test.controller.js
@@ -6,20 +6,38 @@ testController.$inject = [];
 function testController() {
   var vm = this;
   vm.token = JSON.parse(localStorage.getItem('token'));
-  if(!vm.token){
+  if (!vm.token) {
     window.location = '#/login';
   }
-   vm.addtable = [{}];
-   vm.addcol=[{}];
+  vm.addtable = [{}];
+  vm.addcol = [{}];
 
-   vm.add=add;
-   function add(){
-     vm.addtable.push([{}]);
-   }
-
-   vm.addc=addc;
-   function addc(){
-     vm.addcol.push([{}]);
-   }
-
+  vm.add = add;
+  function add() {
+    alert(vm.addtable.length + '-' + vm.addcol.length);
+    vm.addtable.push({});
   }
+
+  vm.addc = addc;
+  function addc() {
+    vm.addcol.push({});
+  }
+
+  vm.addpair = [];
+  vm.pair = pair;
+  function pair() {
+    vm.addpair.push([]);
+  }
+
+  vm.remove=remove;
+  function remove(cbox){
+    console.log(vm.cbox);
+   for(var i=0;i<cbox.length;i++){
+          if(cbox[i].selected==true)
+          {
+            cbox.splice(1);
+          }
+    }
+  }
+
+}
